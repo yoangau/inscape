b0b244440de2cfe72126706d7f29dbe8105ef3e9
Yoan Gauthier <yoangau@gmail.com>
2020-06-25 18:29:02 -0400

    restructure project

--- k3s/init-k3s.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-k3s-uninstall.sh
-sleep 5s
-curl -sfL https://get.k3s.io | sh -
-sleep 1m
-sudo kubectl apply -f traefik.yaml
-sleep 10s
-sudo kubectl get services --all-namespaces
--- k3s/laboulette.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo kubectl create deployment laboulette --image=yoangau/laboulette-server:latest
-sleep 5s
-sudo kubectl create service clusterip laboulette --tcp=80:80
-sleep 5s
-sudo kubectl apply -f ~/cluster/laboulette.yaml
