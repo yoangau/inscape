eeb69dff953f3ac79e67c0e351c4f607e54b6395
Yoan Gauthier <yoangau@gmail.com>
2020-06-25 18:37:32 -0400

    move script

--- init-service.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo cp github-webhooks-service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl start github-webhooks-service
-sudo systemctl enable github-webhooks-service
